bcb5e8c6273ac7edcac6b216ad4a42f329e9d9b6
Justin Schilleman <justin@schilleman.com>
2023-05-18 23:17:50 -0400

    added tailwind.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+   <title>Justin Schilleman | CS@FSU</title>
+</svelte:head>
+
+
+<h1 class="underline text-3xl">Welcome to SvelteKit</h1>
